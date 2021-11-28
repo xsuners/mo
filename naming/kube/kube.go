a6635d63ae1af5579fd5fad453d3f96997c5567d
xsuner <liule@yzsci.com>
2021-11-28 20:28:18 +0800

    a

--- naming/kube/kube.go
+++ naming/kube/kube.go
@@ -8,6 +8,10 @@ type Naming struct{}
 
 var _ naming.Naming = (*Naming)(nil)
 
+func New() naming.Naming {
+	return &Naming{}
+}
+
 func (n *Naming) Deregister() {}
 
 func (n *Naming) Register(svc *naming.Service) (err error) {
