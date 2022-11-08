f485885fffdbc237d7e51a1f093fc01f88e770d1
xsuner <liule@yzsci.com>
2022-11-08 23:40:02 +0800

    a

--- misc/uhttp/cors.go
+++ misc/uhttp/cors.go
@@ -11,14 +11,16 @@ func Cors() gin.HandlerFunc {
 		method := c.Request.Method
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token,x-xsrf-token,Authorization,Token")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, PRI")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
 		if method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusNoContent)
 		}
-
+		if method == "PRI" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
 		c.Next()
 	}
 }
