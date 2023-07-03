1f1849eb8b69692d49454aaebe42e4412c363775
skyasker <xsuner@icloud.com>
2023-07-03 20:55:30 +0800

    a

--- database/xredis/option.go
+++ database/xredis/option.go
@@ -11,8 +11,8 @@ type Options struct {
 
 var defaultOptions = Options{
 	ropts: &redis.Options{
-		Addr:     "127.0.0.1:6379",
-		Password: "PaSsWoRd",
+		Addr: "127.0.0.1:6379",
+		// Password: "PaSsWoRd",
 	},
 }
 
